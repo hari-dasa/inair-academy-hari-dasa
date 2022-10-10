8fbb96968525de759d9962824c9096c6e4c3626c
Hari Dasa <hari.moura@bernoulli.com.br>
2022-10-10 14:59:24 -0300

    change key points at fetching

--- at-scripting-blocks/fetch_one_pokemon.js
+++ at-scripting-blocks/fetch_one_pokemon.js
@@ -1,6 +1,8 @@
 const PokeApi = "https://pokeapi.co/api/v2/";
-table = base.getTable("Pokémon");
-let queryResult = await table.selectRecordsAsync();
+const table = base.getTable("Pokémon");
+let queryResult = await table.selectRecordsAsync({fields: ['fldVVhJlPT0Pfmwos']});
 let record = await input.recordAsync('Pick a Pokemón to get details: ', queryResult);
-let response = await remoteFetchAsync(`${PokeApi}pokemon/${record.name.toLowerCase()}`);
-console.log(await response.json());
+if(record){
+    let response = await remoteFetchAsync(`${PokeApi}pokemon/${record.name.toLowerCase()}`);
+    console.log( await response.json());
+}
