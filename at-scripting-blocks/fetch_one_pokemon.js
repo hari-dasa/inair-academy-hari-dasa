ea80f7b95bbb4845532477d724bc9856090b4895
Hari Dasa <hari.moura@bernoulli.com.br>
2022-10-10 10:35:32 -0300

    removed unnecessary field from fetch script

--- at-scripting-blocks/fetch_one_pokemon.js
+++ at-scripting-blocks/fetch_one_pokemon.js
@@ -1,6 +1,6 @@
 const PokeApi = "https://pokeapi.co/api/v2/";
 table = base.getTable("Pokémon");
-let queryResult = await table.selectRecordsAsync({fields: ['fldVVhJlPT0Pfmwos']});
+let queryResult = await table.selectRecordsAsync();
 let record = await input.recordAsync('Pick a Pokemón to get details: ', queryResult);
 let response = await remoteFetchAsync(`${PokeApi}pokemon/${record.name.toLowerCase()}`);
-console.log( await response.json());
+console.log(await response.json());
